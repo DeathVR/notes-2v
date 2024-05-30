afc22d139211e927902b2db830947466aee0a4af
aomine15122004@gmail.com <aomine15122004@gmail.com>
2024-05-30 15:35:18 +0500

    bloc

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list/domain/provider/app_provider.dart';
 import 'package:todo_list/ui/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,15 +6,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => AppProvider(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          useMaterial3: true,
-        ),
-        home: const HomePage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
       ),
+      home: const HomePage(),
     );
   }
 }
