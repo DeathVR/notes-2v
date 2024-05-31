a58ef26477f4da1776157ca5a75ef38adc98f649
aomine15122004@gmail.com <aomine15122004@gmail.com>
2024-05-31 09:10:23 +0500

    update

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_list/domain/bloc/todo_bloc.dart';
 import 'package:todo_list/ui/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -11,7 +13,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: BlocProvider(
+        create: (context) => TodoBloc(),
+        child: const HomePage(),
+      ),
     );
   }
 }
